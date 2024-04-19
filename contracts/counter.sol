9ee15b47b5856d31c7eab06a748102ed981ef263
mallikarjunreddyD <arjun753016@gmail.com>
2024-04-19 18:30:06 +0530

    Update contract

---
+++ contracts/counter.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+contract counterDAPP {
+    //Create a counter varaible
+    //Do CRUD operations
+
+    uint public counter;
+    event counterUpdate(uint counter);
+
+    constructor(uint _initialValue) {
+        counter = _initialValue;
+    }
+
+    function getCounterValue() public view returns(uint c) {
+        return counter;
+    }
+
+    function incrementCounter() public {
+        counter++;
+        emit counterUpdate(counter);
+    }
+
+    function decrementCounter() public  {
+        counter--;
+        emit counterUpdate(counter);
+    }
+
+    function incrementBy(uint value) public  {
+        counter = counter + value;
+        emit counterUpdate(counter);
+    }
+
+    function decrementBy(uint value) public  {
+        counter = counter - value;
+        emit counterUpdate(counter);
+    }
+}
