5106eca5a9084b787f5f6b2c0c74cf46618fef3c
mallikarjunreddyD <arjun753016@gmail.com>
2024-04-19 19:15:18 +0530

    Add Front-End Boiler

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+function App() {
+
+    return(
+        <div>
+            Welcome
+        </div>
+    )
+}
+
+export default App;
